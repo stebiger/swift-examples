47e9182fb801c67b4580637d6c3195aa75b72bb6
sbi <stefan.billeb@gmx.net>
2015-12-30 21:07:51 +0100

    Add encoder and decoder for serialization

--- FoodTracker/FoodTracker/Meal.swift
+++ FoodTracker/FoodTracker/Meal.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
+
     // MARK: Properties
     var name:   String
     var photo:  UIImage?
     var rating: Int
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
+    // MARK: Archive Paths
+    
+    static let DocumentsDirectory =
+        NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
     
     // MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
@@ -20,10 +33,30 @@ class Meal {
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         if name.isEmpty || rating < 0 {
             return nil
         }
         
     }
+    
+    // MARK: NSCoding
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+    
+        self.init(name: name, photo: photo, rating: rating)
+    }
 
+    
+    
 }
